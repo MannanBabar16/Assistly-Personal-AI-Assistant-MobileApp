a36f863c4579aab9777c1249c8b55ea218b4753e
Abdul Mannan <mannan.babar16@gmail.com>
2024-10-19 19:43:22 +0500

    Firebase Connected

---
+++ android/app/src/main/kotlin/com/example/assistly/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.assistly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+
+
+
